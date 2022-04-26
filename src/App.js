d3742791fd7ba2566e838ecf8ac52e10afa453d1
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-04-27 02:02:58 +0430

    add routers and navigation menue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import react from 'react';
-import './App.css';
+import './assets/App.css';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Header from './components/Header';
+import Quote from './components/Quote';
+import Home from './components/Home';
+import NoPage from './components/NoPage';
 
 class App extends react.Component {
   render() {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+      <>
+        <Header />
+        <Routes className="content">
+          <Route path="/" element={<Home className="content" />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </>
     );
   }
 }
